83c7c57d5e39fabad077e6bb560fa4e1f49074b3
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-05-31 16:58:58 +0530

    ..

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -7,7 +7,7 @@ import Auth from './Landing';
 
 
 
-const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = process.env.PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export function RootLayout() {
     const navigate = useNavigate()
